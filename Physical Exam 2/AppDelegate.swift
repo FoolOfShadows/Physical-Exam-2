7f037dac357d4f02632c560c7a28ae4f0603ae1c
FoolOfShadows <foolofshadows@gmail.com>
2017-12-20 10:46:42 -0600

    Initial Commit

---
+++ Physical Exam 2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Physical Exam 2
+//
+//  Created by Fool on 12/20/17.
+//  Copyright © 2017 Fulgent Wake. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
