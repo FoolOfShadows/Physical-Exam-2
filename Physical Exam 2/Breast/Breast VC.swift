616d4a72967b802f227819bbfc140d73a8ba00e9
Fool <foolofshadows@gmail.com>
2018-04-13 10:56:01 -0500

    Finished addition of feature to switch off norms when abnormals are selected and merged it back into the branch still serving as master (need to figure out how to make this the actual master branch)

--- Physical Exam 2/Breast/Breast VC.swift
+++ Physical Exam 2/Breast/Breast VC.swift
@@ -9,39 +9,39 @@
 import Cocoa
 
 class Breast_VC: NSViewController, ProcessTabProtocol {
-	var selfView = NSView()
-	
+    var selfView = NSView()
+    
 
-	
-	override func viewDidLoad() {
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
-		loadedViewControllers.append(self)
-		selfView = self.view
+        loadedViewControllers.append(self)
+        selfView = self.view
         clearBreast()
         let nc = NotificationCenter.default
         nc.addObserver(self, selector: #selector(selectAllNormsInView), name: NSNotification.Name(rawValue: "SetAllToNorm"), object: nil)
     }
-	
-	@IBAction func processBreastTab(_ sender: Any) {
-		let results = processTab()
-		results.copyToPasteboard()
-		//print(results)
-	}
-	
-	func processTab() -> String {
-		var resultArray = [String]()
-		resultArray.append(Breast().processSectionFrom(getActiveButtonInfoIn(view: self.view)))
-		return resultArray.filter {$0 != ""}.joined(separator: "\n")
-	}
-	
-	@IBAction func clearTab(_ sender: Any) {
-		clearBreast()
-	}
-	
-	func clearBreast() {
-		self.view.clearControllers()
-		self.view.populateSelectionsInViewUsing(Breast())
-	}
+    
+    @IBAction func processBreastTab(_ sender: Any) {
+        let results = processTab()
+        results.copyToPasteboard()
+        //print(results)
+    }
+    
+    func processTab() -> String {
+        var resultArray = [String]()
+        resultArray.append(Breast().processSectionFrom(getActiveButtonInfoIn(view: self.view)))
+        return resultArray.filter {$0 != ""}.joined(separator: "\n")
+    }
+    
+    @IBAction func clearTab(_ sender: Any) {
+        clearBreast()
+    }
+    
+    func clearBreast() {
+        self.view.clearControllers()
+        self.view.populateSelectionsInViewUsing(Breast())
+    }
     
     @IBAction func switchNormOff(_ sender: NSButton) {
         if sender.state != .off {
@@ -70,20 +70,20 @@ class Breast_VC: NSViewController, ProcessTabProtocol {
         }
         
     }
-	
-	@IBAction func selectNorms(_ sender: NSButton) {
-		func normalButtonRangesForSection(_ section:String) -> [Int] {
-			switch section {
-			case "breast":
-				return [Int](1...4)
-			default:
-				return [Int]()
-			}
-		}
-		
-		guard let view = sender.superview else { return }
-		turnButtons(getButtonsInView(view), InRange: normalButtonRangesForSection("breast"), ToState: sender.state)
-	}
+    
+    @IBAction func selectNorms(_ sender: NSButton) {
+        func normalButtonRangesForSection(_ section:String) -> [Int] {
+            switch section {
+            case "breast":
+                return [Int](1...4)
+            default:
+                return [Int]()
+            }
+        }
+        
+        guard let view = sender.superview else { return }
+        turnButtons(getButtonsInView(view), InRange: normalButtonRangesForSection("breast"), ToState: sender.state)
+    }
     
     @objc func selectAllNormsInView() {
         let normButtons = self.view.getNormalButtonsInView()
